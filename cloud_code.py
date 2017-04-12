8104dfc8c09160d119a7a2367e02114a7d7ecf98
Zachary <tszkanlo@gmail.com>
2017-04-13 03:04:51 +0800

    Add headers

--- cloud_code.py
+++ cloud_code.py
@@ -8,9 +8,15 @@ log = logging.getLogger(__name__)
 # Reject empty 'name' before saving a cat to the database
 @skygear.after_save('attendance', async=False)
 def postRequest(record, original_record, db):
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
     log.info('New attendance: ' + record.get('name'))
     data = parse.urlencode({'name': record.get('name'), 'itsc': record.get('itsc'), 'eventid': record.get('event_id'), 'attendanceID': record.id.key}).encode()
-    req =  request.Request('http://api.usthing.xyz/node/ems-email/send-email', data=data) # this will make the method "POST"
+    req =  request.Request('https://api.usthing.xyz/node/ems-email/send-email', data=data, headers=hdr) # this will make the method "POST"
     resp = request.urlopen(req)
     return record
 

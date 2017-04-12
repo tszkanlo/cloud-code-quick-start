456a139e56b60f87b1d678f70eeaa41c1c1776a7
Zachary <tszkanlo@gmail.com>
2017-04-12 22:21:28 +0800

    Testing after save hook

--- cloud_code.py
+++ cloud_code.py
@@ -6,34 +6,32 @@ log = logging.getLogger(__name__)
 
 
 # Reject empty 'name' before saving a cat to the database
-@skygear.before_save('cat', async=False)
+@skygear.after_save('attendance', async=False)
 def validate_cat_name(record, original_record, db):
-    if not record.get('name'):
-        raise Exception('Missing cat name')
-    return record
+    log.info('New attendance: ' + record.get('name'))
 
 
-# cron job that runs every 2 minutes
-@skygear.every('@every 2m')
-def meow_for_food():
-    # Skygear Portal Console Log will show 'Meow Meow!' every 2 minutes
-    log.info('Meow Meow!')
-
-
-# custom logic to be invoked from SDK, e.g.
-# skygear.lambda('food:buy', {'food': 'salmon'}) for JS
-@skygear.op('food:buy', user_required=False)
-def buy_food(food):
-    # TODO: call API about online shopping
-    # return an object to the SDK
-    return {
-        'success': True,
-        'food': food,
-    }
-
-
-# accepting HTTP GET request to /cat/feed
-@skygear.handler('cat:feed', method=['GET'])
-def feed(request):
-    # TODO: handle the request such as logging the request to database
-    return 'Meow! Thanks!\n'
+# # cron job that runs every 2 minutes
+# @skygear.every('@every 2m')
+# def meow_for_food():
+#     # Skygear Portal Console Log will show 'Meow Meow!' every 2 minutes
+#     log.info('Meow Meow!')
+#
+#
+# # custom logic to be invoked from SDK, e.g.
+# # skygear.lambda('food:buy', {'food': 'salmon'}) for JS
+# @skygear.op('food:buy', user_required=False)
+# def buy_food(food):
+#     # TODO: call API about online shopping
+#     # return an object to the SDK
+#     return {
+#         'success': True,
+#         'food': food,
+#     }
+#
+#
+# # accepting HTTP GET request to /cat/feed
+# @skygear.handler('cat:feed', method=['GET'])
+# def feed(request):
+#     # TODO: handle the request such as logging the request to database
+#     return 'Meow! Thanks!\n'

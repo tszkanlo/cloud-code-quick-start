b9b0c4799a7f9bac7b941d1fc757c69122e02eae
Zachary <tszkanlo@gmail.com>
2017-08-31 14:37:40 +0800

    Modify for new skygear server version

--- cloud_code.py
+++ cloud_code.py
@@ -1,7 +1,9 @@
 import skygear
 import logging
 from urllib import request, parse
-from skygear.utils.user import reset_password_by_username
+import bcrypt
+from sqlalchemy.sql import text
+from .db import conn
 
 log = logging.getLogger(__name__)
 
@@ -28,6 +30,34 @@ def custom_reset_password(username, new_password):
     return {
         'success': is_success,
     }
+
+def hash_password(password):
+    """
+    Return a hased password of a plaintext password.
+    """
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed.decode("utf-8")
+
+
+def reset_password_by_username(username, new_password):
+    """
+    Reset the user password with a new password.
+    """
+    if not (isinstance(username, str) and isinstance(new_password, str)):
+        raise ValueError("username and new_password must be string")
+
+    sql = text('''
+        UPDATE \"_auth\"
+        SET password = :new_password
+        WHERE id = (SELECT _id
+                    FROM user
+                    WHERE username = :username)
+        ''')
+    with conn() as db:
+        result = db.execute(sql,
+                            new_password=hash_password(new_password),
+                            username=username)
+    return result.rowcount > 0
 
 # # cron job that runs every 2 minutes
 # @skygear.every('@every 2m')

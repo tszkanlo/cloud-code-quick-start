7798605360067c6b5cb9b2ce8d3a5df653c5fdc2
Zachary <tszkanlo@gmail.com>
2017-08-31 15:00:12 +0800

    deployed

--- cloud_code.py
+++ cloud_code.py
@@ -58,28 +58,3 @@ def reset_password_by_username(username, new_password):
                             new_password=hash_password(new_password),
                             username=username)
     return result.rowcount > 0
-
-# # cron job that runs every 2 minutes
-# @skygear.every('@every 2m')
-# def meow_for_food():
-#     # Skygear Portal Console Log will show 'Meow Meow!' every 2 minutes
-#     log.info('Meow Meow!')
-#
-#
-# # custom logic to be invoked from SDK, e.g.
-# # skygear.lambda('food:buy', {'food': 'salmon'}) for JS
-# @skygear.op('food:buy', user_required=False)
-# def buy_food(food):
-#     # TODO: call API about online shopping
-#     # return an object to the SDK
-#     return {
-#         'success': True,
-#         'food': food,
-#     }
-#
-#
-# # accepting HTTP GET request to /cat/feed
-# @skygear.handler('cat:feed', method=['GET'])
-# def feed(request):
-#     # TODO: handle the request such as logging the request to database
-#     return 'Meow! Thanks!\n'
